9849156102a6326f902577f89a011f206f823d31
Yihong Chen <yihongc@twitter.com>
2017-11-15 17:09:54 -0800

    Fix the bug if add DedupLink

--- src/graphql/link.js
+++ src/graphql/link.js
@@ -1,9 +1,11 @@
 import { ApolloLink } from 'apollo-link'
+import { DedupLink } from "apollo-link-dedup"
 import { RetryLink } from 'apollo-link-retry'
 import { HttpLink } from 'apollo-link-http'
 
 export const link = ApolloLink.from([
   new RetryLink({ max: 4 }),
+  new DedupLink(),
   new HttpLink({
     uri: 'https://api.graph.cool/simple/v1/cixmkt2ul01q00122mksg82pn'
   })
